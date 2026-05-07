f77a4dffd1e902db38f94893e4168f45207eb41e
Cindy Liu <cindyliu@Cindys-MacBook-Air.local>
2026-05-07 19:34:16 -0400

    update

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,11 +1,14 @@
 public class Multiples {
     public static void main(){
+        System.out.println(count(1000));
+    }
+    private static int count(int limit){
         int count = 0;
         for (int i = 1; i < 1000; i++){
             if (i%3 == 0 || i%5 == 0){
                 count++;
             }
         }
-        System.out.println(count);
+        return count;
     }
 }
